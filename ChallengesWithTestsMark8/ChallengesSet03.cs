8ac2413f7f056c90e72cf30218d19941e925b920
d3lliott <d3elliott2@gmail.com>
2021-03-01 19:52:27 -0500

    week 4 done

--- ChallengesWithTestsMark8/ChallengesSet03.cs
+++ ChallengesWithTestsMark8/ChallengesSet03.cs
@@ -1,39 +1,101 @@
 ﻿using System;
 using System.Collections.Generic;
 
+
 namespace ChallengesWithTestsMark8
 {
     public class ChallengesSet03
     {
         public bool ArrayContainsAFalse(bool[] vals)
         {
-            throw new NotImplementedException();
+            int y = 1;
+            foreach(bool x in vals)
+            {
+                
+                    if (x == false)
+                    {
+                        y++;
+                    }
+                    else
+                    {
+                        y = y + 0;
+                    }
+            }
+            if (y >= 2)
+                return true;
+            else
+                return false;
         }
 
         public bool IsSumOfOddsOdd(IEnumerable<int> numbers)
         {
-            throw new NotImplementedException();
+            int x = 0;
+            if (numbers == null)
+                return false;
+
+            foreach (int z in numbers)
+            {
+                if (z % 2 != 0)
+                    x += z;
+                else
+                    x += 0;
+            }
+
+            if (x % 2 != 0)
+                return true;
+            else
+                return false;
+            
         }
 
         public bool PasswordContainsUpperLowerAndNumber(string password)
         {
-            throw new NotImplementedException();
+            bool x = false;
+            bool y = false;
+            bool z = false;
+            for (int i = 0; i < password.Length; i++)
+            {
+                if (char.IsUpper(password[i]))
+                    x = true;
+
+                if (char.IsLower(password[i]))
+                    y = true;
+                if (char.IsNumber(password[i]))
+                    z = true;
+
+            }
+            if (x == true && y == true && z == true)
+                return true;
+            else
+                return false;
         }
 
         public char GetFirstLetterOfString(string val)
         {
-            throw new NotImplementedException();
+            char[] arr = val.ToCharArray();
+            char y = arr[0];
+            return y; ;
         }
 
         public char GetLastLetterOfString(string val)
         {
-            throw new NotImplementedException();
+            char[] arr = val.ToCharArray();
+            char y = arr[arr.Length - 1];
+            return y;
+           ;
         }
 
         public decimal Divide(decimal dividend, decimal divisor)
         {
-           
-            decimal x = dividend / divisor;
+            decimal x;
+            if (divisor == 0)
+            {
+                return 0;
+            }
+            else
+            {
+                x = dividend / divisor;
+            }
             return x;
         }
 
@@ -45,12 +107,32 @@ namespace ChallengesWithTestsMark8
 
         public int[] GetOddsBelow100()
         {
-            throw new NotImplementedException();
+            int x = 2;
+            int z = 0;
+            int[] y = new int[50];
+            do
+            {
+                y[z++] = (z * x) - 1;
+            } while (z < 50);
+            return y;
+            
+            
+            
         }
 
         public void ChangeAllElementsToUppercase(string[] words)
         {
-            throw new NotImplementedException();
+
+            // for (int i = 0; i < words.Length; i++)
+            //   words[i]= words[i].ToUpper();
+            int x = 0;
+            foreach (string i in words)
+            {
+                words[x] = words[x].ToUpper();
+                x++;
+            }   
+            
+           
         }
     }
 }
--- ChallengesWithTestsMark8/ChallengesSet04.cs
+++ ChallengesWithTestsMark8/ChallengesSet04.cs
@@ -6,47 +6,140 @@ namespace ChallengesWithTestsMark8
     {
         public int AddEvenSubtractOdd(int[] numbers)
         {
-            throw new NotImplementedException();
+            int x = 0;
+            foreach (int i in numbers)
+            { if (i % 2 == 0)
+                    x += i;
+                else
+                    x -= i;
+
+            }
+            return x;
         }
 
         public int GetLengthOfShortestString(string str1, string str2, string str3, string str4)
         {
-            throw new NotImplementedException();
+            int x;
+            if (str1.Length < str2.Length)
+                x = str1.Length;
+            else
+                x = str2.Length;
+
+            if (str3.Length < x)
+                x = str3.Length;
+
+            if (str4.Length < x)
+                x = str4.Length;
+
+            return x;
+
+            
         }
 
         public int GetSmallestNumber(int number1, int number2, int number3, int number4)
         {
-            throw new NotImplementedException();
+            int x;
+
+            if (number1 < number2)
+                x = number1;
+            else
+                x = number2;
+
+            if (number3 < x)
+                x = number3;
+
+            if (number4 < x)
+                x = number4;
+
+            return x;
         }
 
         public void ChangeBusinessNameTo_TrueCoders(Business biz)
         {
-            throw new NotImplementedException();
+            biz.Name = "TrueCoders";
+            Console.WriteLine(biz.Name);
+
+
+            
         }
 
         public bool CouldFormTriangle(int sideLength1, int sideLength2, int sideLength3)
         {
-            throw new NotImplementedException();
+            if (sideLength1 + sideLength2 <= sideLength3 || sideLength2 + sideLength3 <= sideLength1 || sideLength1 + sideLength3 <= sideLength2)
+                return false;
+            else
+                return true;
+            
+          
         }
 
         public bool IsStringANumber(string input)
         {
-            throw new NotImplementedException();
+            int x = 0;
+            if (input == null || input.Length == 0)
+                return false;
+            foreach (char c in input)
+                if (c == '0' || c == '1' || c == '2' || c == '3' || c == '4' || c == '5' || c == '6' || c == '7' || c == '8' || c == '9' || c == '-' || c == '.')
+                    x += 0;
+                else
+                    x += 1;
+
+            if (x == 0)
+                return true;
+            else
+                return false;
         }
 
         public bool MajorityOfElementsInArrayAreNull(object[] objs)
         {
-            throw new NotImplementedException();
+            int x = 0;
+            int y = 0;
+           for(int i = 0; i < objs.Length; i++)
+            {
+                if (objs[i] == null)
+                    x += 1;
+                else
+                    y += 1;
+            }
+            if (x > y)
+                return true;
+            else
+                return false;
         }
 
         public double AverageEvens(int[] numbers)
         {
-            throw new NotImplementedException();
+            if (numbers == null || numbers.Length == 0)
+                return 0;
+            double y = 0;
+            double x = 0;
+
+            foreach (double i in numbers)
+                if (i % 2 == 0)
+                {
+                    x += i;
+                    y += 1;
+                }
+            if (x == 0)
+                return 0;
+
+            return x/y;
         }
 
         public int Factorial(int number)
         {
-            throw new NotImplementedException();
+            int x = number;
+            if (number < 0)
+                throw new ArgumentOutOfRangeException();
+            if (number == 0)
+                return 1;
+            for (int i = number - 1; i > 0; i--)
+                x *= i;
+
+            return x;
+
+
+                
         }
     }
 }
